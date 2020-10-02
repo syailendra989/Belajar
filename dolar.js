168161490187e0578ee8a8ac67f7aae8e78bfa53
syailendra989 <71954685+syailendra989@users.noreply.github.com>
2020-10-02 11:20:02 +0700

    dolar

---
+++ dolar.js
@@ -0,0 +1,31 @@
+//bintang dolar
+var baris = 3;
+var polaBintang = "*";
+var dolar = "$"
+var polaSamaDengan = "=";
+var hasil = "";
+
+console.log(baris)
+for (let index = 2; index < 5; index++) {
+
+  if ((index % 2) == 0) {
+    hasil = hasil.concat(polaSamaDengan);
+  } else {
+    hasil = hasil.concat(polaBintang);
+  }
+
+}
+console.log("" + dolar + hasil + dolar);
+
+hasil = "";
+
+for (let index = 2; index < 3; index++) {
+  if ((index % 2) == 0) {
+    hasil = hasil.concat(polaSamaDengan);
+  } else {
+    hasil = hasil.concat(polaBintang);
+  }
+
+}
+console.log(" " + dolar+ hasil+dolar);
+console.log("  " + dolar);
