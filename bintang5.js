a54b01ba7a0f49f31f4ec136f5b7030c25176c93
syailendra989 <71954685+syailendra989@users.noreply.github.com>
2020-10-06 19:02:14 +0700

    Update bintang5.js

--- bintang5.js
+++ bintang5.js
@@ -81,3 +81,28 @@ for (let index = 1; index<4; index++){
 }
 console.log(hasil);
 console.log("    "+polaBintang);
+
+//cepat 
+var baris=10;
+var polaBintang= "*";
+var polaSamaDengan= "=";
+var spasi =  "";
+
+for(let index = baris; index > 0; index--){
+
+    var hasil="";
+
+    var jmplhcell = (index*2)-1;
+    //console.log(jmplhcell);
+
+    for (let index2 = jmplhcell; index2 > 0; index2--){
+        if ((index2 %2)==0){
+         hasil = hasil.concat(polaSamaDengan);
+        } else{
+         hasil = hasil = hasil.concat(polaBintang);
+        } 
+   
+    }
+    console.log(spasi+hasil)
+    spasi += " ";
+} 
